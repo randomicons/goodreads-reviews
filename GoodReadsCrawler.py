1b6c9d3a9cdc8f18f1e01c26055aed0d665e8e60
randomicons <srirambhat7@gmail.com>
2018-12-08 13:16:13 -0600

    Changed number of reviews and books

--- GoodReadsCrawler.py
+++ GoodReadsCrawler.py
@@ -10,6 +10,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec
 
+num_revs_per_book = 200
+num_books = 30
+df_revs = pd.DataFrame(columns=["book", "author", "rating", "review"])
+
 
 def main():
     userid_str, password_str = (sys.argv[1], sys.argv[2])
@@ -30,14 +34,15 @@ def main():
         create_book_links(driver)
 
     with open("book_links.txt", "r") as file:
-        links = file.readlines()
+        links = file.readlines()[0:num_books]
 
-    df_revs = pd.DataFrame(columns=["book", "author", "rating", "review"])
-    get_reviews(links[0], driver, df_revs, num_reviews=0)
+    rev_i = 0
+    for link in links:
+        rev_i = get_reviews(link, driver, df_revs, rev_i)
     df_revs.to_csv("data.csv")
 
 
-def get_reviews(book_url, driver, df_revs, num_reviews):
+def get_reviews(book_url, driver, df_revs, rev_i):
     driver.get(book_url)
     driver.implicitly_wait(10)
     bookTitle = driver.find_element_by_id("bookTitle").get_attribute("innerHTML")
@@ -45,8 +50,9 @@ def get_reviews(book_url, driver, df_revs, num_reviews):
     wait = WebDriverWait(driver, 10)
     # Press all more to expand reviews
     # mores = driver.find_elements_by_xpath('//a[contains(text(), "{0}") and @class="inner"]'.format("...more"))
+    rev_cnt = 0
 
-    while num_reviews < 10:
+    while rev_cnt < num_revs_per_book:
         driver.implicitly_wait(10)
         els = driver.find_elements_by_class_name("friendReviews")
         driver.implicitly_wait(0)
@@ -67,21 +73,23 @@ def get_reviews(book_url, driver, df_revs, num_reviews):
             except NoSuchElementException:
                 pass
 
-           # Wait for clicking more to load rest of the review
+            # Wait for clicking more to load rest of the review
             # IndexError occurs on reviews with no text just a rating which are skipped
             try:
                 wait.until(lambda _: el.find_elements_by_css_selector("span[id^='freeTextContainer'")[0].get_attribute(
                     "innerHTML").strip() != "")
                 texts = el.find_elements_by_css_selector("span[id^='freeTextContainer'")
                 text = texts[0].get_attribute("innerHTML")
-                print(text)
-                df_revs.loc[num_reviews] = (bookTitle.strip(), first_author.strip(), num_stars, text.strip())
+                # print(text)
+                df_revs.loc[rev_i] = (bookTitle.strip(), first_author.strip(), num_stars, text.strip())
             except IndexError:
                 continue
-            num_reviews += 1
+            rev_cnt += 1
+            rev_i += 1
         driver.find_element_by_class_name("next_page").click()
-        wait.until(ec.staleness_of(els[0]))
-    print(num_reviews)
+        wait.until(ec.staleness_of(els[5]))
+    print(rev_cnt)
+    return rev_i
 
 
 def create_book_links(driver):
