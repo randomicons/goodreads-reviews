0c366f044926cbce626955e9736a3ac712074eb0
randomicons <srirambhat7@gmail.com>
2018-12-09 18:08:22 -0600

    Removed html tags

--- clean.py
+++ clean.py
@@ -3,19 +3,27 @@ from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 
 
-def clean(df):
+def remove_non_english(df):
     # Remove redundant column headers
     df = df[df['book'] != 'book']
+    # Does the same as: (but this cannot do exceptions)
+    # df = df[df.apply(lambda x: detect(x['review']) == 'en', axis=1)]
     keep_index = []
     for i in range(len(df)):
         try:
             keep_index.append(detect(df.iloc[i]['review']) == 'en')
         except LangDetectException:
             keep_index.append(False)
-    df = df[keep_index]
+    return df[keep_index]
 
+
+def remove_html_tags(df):
+    from bs4 import BeautifulSoup as bs
+    df['review'] = df.apply(lambda x: bs(x['review']).get_text(separator=" ", strip=True), axis=1)
+    return df
 
 
 if __name__ == '__main__':
     df = pd.read_csv("data.csv", index_col=['index'])
-    clean(df)
+    df = remove_html_tags(remove_non_english(df))
+    df.to_csv("data.csv")
