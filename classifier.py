f0cfeacea21234ac9c3010e1acff352ecb4812fc
shrinathshankar <36801239+shrinathshankar@users.noreply.github.com>
2018-12-13 12:32:07 -0600

    help

---
+++ classifier.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+
+def svm():
+
+    data = pd.read_csv("Data/data.csv")
+    x = data.drop('rating', axis=1)
+    y = data['rating']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+    svclassifier = SVC(kernel='linear')
+    svclassifier.fit(x_train, y_train)
+
+
+if __name__ == 'main' :
+    svm()
