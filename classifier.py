631086e9f1a29f7219a515cd4523972e717e6019
shrinathshankar <36801239+shrinathshankar@users.noreply.github.com>
2018-12-13 22:37:18 -0600

    SVM classifying data

--- classifier.py
+++ classifier.py
@@ -1,17 +1,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-import matplotlib.pyplot as plt
+from util import run_or_get_pkl
 
 
 def svm():
 
-    data = pd.read_csv("Data/data.csv")
-    x = data.drop('rating', axis=1)
-    y = data['rating']
+    data = run_or_get_pkl("bow_all.p")
+    rating = pd.read_csv("data.csv")
+    x = data
+    y = rating['rating']
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
-    svclassifier = SVC(gamma='scale', decision_function_shape='ovo', kernel='linear')
+    svclassifier = SVC(gamma='scale', decision_function_shape='ovo', kernel='rbf')
     svclassifier.fit(x_train, y_train)
 
 
