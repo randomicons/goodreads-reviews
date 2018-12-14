1f357c468ea202f138d4b3898a39b17af40d1824
shrinathshankar <36801239+shrinathshankar@users.noreply.github.com>
2018-12-13 22:02:28 -0600

    SVM classifying data

--- classifier.py
+++ classifier.py
@@ -11,12 +11,8 @@ def svm():
     y = data['rating']
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
-    svclassifier = SVC(kernel='linear')
+    svclassifier = SVC(gamma='scale', decision_function_shape='ovo', kernel='linear')
     svclassifier.fit(x_train, y_train)
-
-    clf = SVC(gamma='scale', decision_function_shape='ovo')
-    clf.fit(x, y)
-
 
 
 if __name__ == 'main' :
